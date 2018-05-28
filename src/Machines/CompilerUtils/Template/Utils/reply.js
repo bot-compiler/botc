b7c67a7134adce58a6e57c06372643633623dabb
Abhishek V <abhivijay96@gmail.com>
2018-05-28 06:53:08 +0530

    sessionId in index

--- src/Machines/CompilerUtils/Template/Utils/reply.js
+++ src/Machines/CompilerUtils/Template/Utils/reply.js
@@ -1,5 +1,5 @@
 var data = require('../data')
 
 module.exports = function(uuid, text){
-    data[uuid]['res'].send(JSON.stringify({"speech": text, "displayText": text}))
+    data[uuid]['res'].send(JSON.stringify({"fulfillmentText": text}))
 }
